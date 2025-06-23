99df867f8505f28b8233df9b17e5b9f92ec43c12
pseudocat <sydneyang1234@gmail.com>
2025-06-23 18:41:38 +0800

    feat: create a view file for pending approvals

---
+++ resources/js/pages/SuperAdmin/PendingApprovals.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link } from '@inertiajs/vue3';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Pending Approvals',
+        href: '/pending/approvals',
+    },
+];
+</script>
+
+<template>
+    <Head title="Pending Approvals" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div>
+                <!-- Table -->
+                <Table>
+                    <TableCaption>A list of all the pending admin approvals.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead class="w-[100px]">
+                                Admin ID
+                            </TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Email</TableHead>
+                            <TableHead class="text-center">
+                                Action
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell class="font-medium">
+                                INV001
+                            </TableCell>
+                            <TableCell>Paid</TableCell>
+                            <TableCell>Credit Card</TableCell>
+                            <TableCell class="grid grid-cols-3 text-center">
+                                <Link href="#" class="-mr-64 cursor-pointer">Edit</Link>
+                                <Link href="#" class="text-green-500 cursor-pointer">Accept</Link>
+                                <Link href="#" class="-ml-64 text-red-500 cursor-pointer">Delete</Link>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    </AppLayout>
+</template>
